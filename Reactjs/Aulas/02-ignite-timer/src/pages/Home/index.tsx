0d446f5949a594908f654a1508e034f3b055c294
Lucas Angeli <jl.angeli@outlook.com>
2023-11-22 11:55:18 -0300

    feat: added task cycle logic and states

--- Reactjs/Aulas/02-ignite-timer/src/pages/Home/index.tsx
+++ Reactjs/Aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -11,6 +11,7 @@ import {
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
+import { useState } from 'react';
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -22,7 +23,16 @@ const newCycleFormValidationSchema = zod.object({
 
 type newCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
+interface Cycle {
+  id: string;
+  task: string;
+  minutesAmount: number;
+}
+
 export default function Home() {
+  const [cycles, setCycles] = useState<Cycle[]>([]);
+  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+
   const { register, handleSubmit, watch, reset } = useForm<newCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
@@ -32,8 +42,21 @@ export default function Home() {
   });
 
   const handleCreateNewCycle = (data: newCycleFormData) => {
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+    };
+
+    setCycles((state) => [...state, newCycle]); //Sempre que a atualização do estado depender do estado anterior utilizar uma arrow function para fazer a nova atribuição.
+    setActiveCycleId(id);
+
     reset();
   };
+
+  const findActiveCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   const taskIsValid = watch('task');
   const isSubmitDisabled = !taskIsValid;
